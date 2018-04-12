2f4df63ae1dca2380352145f167ae6c6088e52bf
tsuyoshi komuro <komuro.0418@gmail.com>
2018-04-12 22:55:08 +0900

    twitter card

--- header.php
+++ header.php
@@ -28,6 +28,11 @@
 		<meta propaty="og:site_name" content="<?php bloginfo( 'name' ); ?>">
 		<meta propaty="og:locale" conten="ja_JP">
 
+		<!-- twitter card -->
+		<meta name="twitter:site" content="@ukkari_ukachan">
+		<meta name="twitter:card" content="summary_large_image">
+		<!-- twitter card ここまで -->
+
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
