dbbae36f4ca4557b2190de4fb1dbf33bcef10013
tsuyoshi komuro <komuro.0418@gmail.com>
2018-04-12 20:19:03 +0900

    ogp設定

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
+    <head prefix="og: http://ogp.me/ns#">
         <meta charset="UTF-8">
         <title>
             <?php wp_title( '|', true, 'right' ); ?>
@@ -10,6 +10,12 @@
         <!-- <link rel="stylesheet" href="//maxcdn.boostrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> -->
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?ver=<?php echo date('U'); ?>">
+        <?php if( is_single() ): // 記事の個別ページ用のメタデータ ?>
+        <meta propaty="og:type" content="article">
+        <meta propaty="og:title" content="<?php the_title(); ?>">
+        <meta propaty="og:url" content="<?php the_permaling(); ?>">
+        <meta propaty="og:description" content="<?php echo wp_trim_words( $post->post_content, 100, '･･･' ); ?>">
+        <?php endif; // 記事の個別ページ用のメタデータ[ここまで] ?>
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
