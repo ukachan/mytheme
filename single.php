0ef16d0135ebf51755d539e0b7b79e18ae553419
tsuyoshi komuro <komuro.0418@gmail.com>
2018-04-13 22:56:39 +0900

    h tags add

--- single.php
+++ single.php
@@ -54,15 +54,15 @@
 		<ul>
 			<li><a href="http://twitter.com/intent/tweet?text=<?php echo urlencode( get_the_title() . ' - '. get_bloginfo( 'name' ) ); ?>&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;via=ebisucom" onclick="window.open(this.href, 'SNS', 'width=500, height=300, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-tw">
 				<i class="fa fa-twitter"></i>
-				Twitter でシェア
+				<span>Twitter</span> でシェア
 			</a></li>
 			<li><a href="http://www.facebook.com/share.php?u=<?php echo urlencode( get_permalink() ); ?>" onclick="window.open(this.href, 'SNS', 'width=500, height=500, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-fb">
 				<i class="fa fa-facebook"></i>
-				Facebookで シェア
+				<span>Facebook</span> でシェア
 			</a></li>
 			<li><a href="https://plus.google.com/share?url=<?php echo urlencode( get_permalink() ); ?>" onclick="window.open(this.href, 'SNS', 'window=500, height=500, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-gp">
 				<i class="fa fa-google-plus"></i>
-				Google+ でシェア
+				<span>Google+</span> でシェア
 			</a></li>
 		</ul>
 	</div>
