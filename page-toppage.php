e2a7c7f9a456da602a0ddeece71f559062c29514
tsuyoshi komuro <komuro.0418@gmail.com>
2018-05-06 16:42:52 +0900

    fix

---
+++ page-toppage.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<div class="container">
+    <div class="contents">
+        CONTENTS
+    </div>
+    <div class="sub">
+        sidebar
+    </div>
+</div>
+
+<?php get_footer(); ?>
