9a3af52b3699c61f05cdd6fdb28f37a94b78dfb8
tsuyoshi komuro <komuro.0418@gmail.com>
2018-05-07 09:50:21 +0900

    conpany-style add

--- page-toppage.php
+++ page-toppage.php
@@ -1,7 +1,30 @@
 <?php get_header(); ?>
 <div class="container">
     <div class="contents">
-        CONTENTS
+    <?php
+        $location_name = 'pagenav';
+        $locations = get_nav_menu_locations();
+        $myposts = wp_get_nav_menu_items( $locations[ $location_name ] );
+        if( $myposts ): ?>
+        <aside class="mymenu mymenu-top">
+        <ul>
+            <?php foreach($myposts as $post):
+            if( $post->object == 'page' ):
+                $post = get_post( $post->object_id );
+            setup_postdata($post); ?>
+            <li><a href="<?php the_permalink(); ?>">
+            <div class="thumb" style="background-image: url(<?php echo mythumb( 'medium' ); ?>"></div>
+            <div class="text">
+            <h2><?php the_title(); ?></h2>
+            <?php the_excerpt(); ?>
+            </div>
+            </a></li>
+            <?php endif;
+            endforeach; ?>
+        </ul>
+        </aside>
+        <?php wp_reset_postdata();
+        endif; ?>
     </div>
     <div class="sub">
         sidebar
